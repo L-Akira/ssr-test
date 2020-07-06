a31fe4abd6087eac0374b7efea126c3191e6ba1f
Leonardo Akira <leonardoakira08@gmail.com>
2020-07-06 03:16:08 -0300

    simple server side rendering test

---
+++ README.md
@@ -0,0 +1,3 @@
+## ⭐Simple server side rendering test ⭐
+
+It's running by defaut at port 5000
---
+++ Server/server.js
@@ -0,0 +1,38 @@
+import {config} from 'dotenv'
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import fs from 'fs';
+import path from 'path';
+import FrontRouter from '../test-app/src/router';
+
+config();
+
+const app = express();
+
+app.use(express.static('../test-app/build'))
+
+app.get('/*',(req,res)=>{
+    const context = {}
+    console.log('request made');
+    
+    const load = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <FrontRouter/>
+        </StaticRouter>
+    );
+
+    const HTML = path.resolve('../test-app/build/index.html');
+
+    fs.readFile(HTML,'utf8',(err,data)=>{
+        if(err)
+            return res.status(500).send('Error 500: Sorry for the inconvinence');
+
+        return res.send(
+            data.replace('<div id="root"></div>',`<div id="root">${load}</div>`)
+        );
+    });   
+})
+
+app.listen(process.env.PORT||5000,()=>{console.log(`Listening at ${process.env.PORT||5000}`)});
